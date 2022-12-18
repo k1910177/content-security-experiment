9f8c9032ad21aa073535b011d0948735982e4886
k1910177 <k1910177@gl.cc.uec.ac.jp>
2022-12-18 11:19:00 +0900

    use conn module

--- cmd/kadai2-solo/main.go
+++ cmd/kadai2-solo/main.go
@@ -2,59 +2,129 @@ package main
 
 import (
 	"contentssecurity/pkg/table"
+	"time"
 
 	conn "github.com/uecconsecexp/secexp2022/se_go/connector"
 )
 
 func main() {
-	// Chugaku
-	A, _ := table.Import("data/seiseki.txt")
+	yc := make(chan bool)
+	go func() {
+		yobikouSide()
+		yc <- true
+	}()
 
-	// Yobikou
-	B, _ := table.Import("data/omomi.txt")
-	C, _ := table.Import("data/saiteiten.txt")
+	time.Sleep(500 * time.Millisecond)
 
-	// Create random matrix M
-	MRows, MCols := len(A.Cols), len(B.Rows)
-	M := table.Random(MRows, MCols)
+	cc := make(chan bool)
+	go func() {
+		chugakuSide("0.0.0.0")
+		cc <- true
+	}()
 
-	// Send M and calculate M inverse
-	MInv := M.Inv()
-
-	// Slice M
-	MLeft, MRight := M.Slice(0, MRows, 0, MCols/2), M.Slice(0, MRows, MCols/2, MCols)
-	MInvTop, MInvBottom := MInv.Slice(0, MRows/2, 0, MCols), MInv.Slice(MRows/2, MRows, 0, MCols)
-
-	// Calculate and send A' and B'
-	APrime := A.Mul(MLeft)
-	BPrime := MInvBottom.Mul(B)
-
-	// Calculate and send A'' and B''
-	APrimePrime := A.Mul(MRight).Mul(BPrime)
-	BPrimePrime := APrime.Mul(MInvTop).Mul(B)
-
-	// Calculate aptitude and evaulate result
-	aptitudeTable := APrimePrime.Add(BPrimePrime)
-	resultTable := aptitudeTable.Evaluate(C)
-
-	// Export
-	aptitudeTable.Export("bin/tekisei.txt", "適性")
-	resultTable.ExportResult("bin/kekka.txt", "合否")
-}
-
-func yobikouSide() {
-	yobikou, err := conn.NewYobikouServer()
-	if err != nil {
-		panic(err)
-	}
-	defer yobikou.Close()
+	<-yc
+	<-cc
 }
 
 func chugakuSide(addr string) {
-	chugaku, err := conn.NewChugakuClient(addr)
+	client, err := conn.NewChugakuClient(addr)
 	if err != nil {
 		panic(err)
 	}
-	defer chugaku.Close()
+	defer client.Close()
 
+	// Import seiseki table
+	A, _ := table.Import("data/seiseki.txt")
+
+	// Create random matrix M
+	MRows, MCols := len(A.Cols), len(A.Cols)
+	M := table.Random(MRows, MCols)
+
+	// Send M to yobikou
+	if err = M.ChugakuSend(&client); err != nil {
+		panic(err)
+	}
+
+	// Slice M
+	MLeft, MRight := M.Slice(0, MRows, 0, MCols/2), M.Slice(0, MRows, MCols/2, MCols)
+
+	// Calculate and send A'
+	APrime := A.Mul(MLeft)
+	if err = APrime.ChugakuSend(&client); err != nil {
+		panic(err)
+	}
+
+	// Wait and receive B'
+	BPrime, err := table.ChugakuReceive(&client, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Calculate and send A''
+	APrimePrime := A.Mul(MRight).Mul(BPrime)
+	if err = APrimePrime.ChugakuSend(&client); err != nil {
+		panic(err)
+	}
+
+	// Wait and receive result
+	result, err := table.ChugakuReceive(&client, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Export result
+	result.ExportResult("bin/kekka.txt", "合否")
+}
+
+func yobikouSide() {
+	server, err := conn.NewYobikouServer()
+	if err != nil {
+		panic(err)
+	}
+	defer server.Close()
+
+	B, _ := table.Import("data/omomi.txt")
+	C, _ := table.Import("data/saiteiten.txt")
+
+	// Wait and receive M
+	M, err := table.YobikouReceive(&server, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Calculate M' and slice
+	MInv := M.Inv()
+	MRows, MCols := len(M.Rows), len(M.Cols)
+	MInvTop, MInvBottom := MInv.Slice(0, MRows/2, 0, MCols), MInv.Slice(MRows/2, MRows, 0, MCols)
+
+	// Wait and receive A'
+	APrime, err := table.YobikouReceive(&server, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Calculate and send B'
+	BPrime := MInvBottom.Mul(B)
+	if err = BPrime.YobikouSend(&server); err != nil {
+		panic(err)
+	}
+
+	// Calculate and send B''
+	BPrimePrime := APrime.Mul(MInvTop).Mul(B)
+
+	// Wait and receive A''
+	APrimePrime, err := table.YobikouReceive(&server, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Calculate and export aptitude
+	aptitude := APrimePrime.Add(BPrimePrime)
+	aptitude.Export("bin/tekisei.txt", "適性")
+
+	// Evaluate and send result
+	result := aptitude.Evaluate(C)
+	if err = result.YobikouSend(&server); err != nil {
+		panic(err)
+	}
 }
---
+++ pkg/table/conn.go
@@ -0,0 +1,28 @@
+package table
+
+import conn "github.com/uecconsecexp/secexp2022/se_go/connector"
+
+func (table *Table) ChugakuSend(client *conn.ChugakuClient) error {
+	return client.SendTable(table.ToSlice())
+}
+
+func ChugakuReceive(client *conn.ChugakuClient, rows []string, cols []string) (*Table, error) {
+	data, err := client.ReceiveTable()
+	if err != nil {
+		return nil, err
+	}
+	return New(data, rows, cols), nil
+}
+
+func (table *Table) YobikouSend(server *conn.YobikouServer) error {
+	return server.SendTable(table.ToSlice())
+}
+
+func YobikouReceive(server *conn.YobikouServer, rows []string, cols []string) (*Table, error) {
+	data, err := server.ReceiveTable()
+	if err != nil {
+		return nil, err
+	}
+
+	return New(data, rows, cols), nil
+}
