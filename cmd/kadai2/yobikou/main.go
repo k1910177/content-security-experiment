3ef5e2264fa0af1e98b2c62caf2371b9fb5a21b2
k1910177 <118232271+k1910177@users.noreply.github.com>
2022-12-20 12:47:45 +0900

    kadai2 (#4)

--- cmd/kadai2/yobikou/main.go
+++ cmd/kadai2/yobikou/main.go
@@ -2,81 +2,11 @@ package main
 
 import (
 	"contentssecurity/pkg/table"
-	"time"
 
 	conn "github.com/uecconsecexp/secexp2022/se_go/connector"
 )
 
 func main() {
-	yc := make(chan bool)
-	go func() {
-		yobikouSide()
-		yc <- true
-	}()
-
-	time.Sleep(500 * time.Millisecond)
-
-	cc := make(chan bool)
-	go func() {
-		chugakuSide("0.0.0.0")
-		cc <- true
-	}()
-
-	<-yc
-	<-cc
-}
-
-func chugakuSide(addr string) {
-	client, err := conn.NewChugakuClient(addr)
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-
-	// Import seiseki table
-	A, _ := table.Import("data/seiseki.txt")
-
-	// Create random matrix M
-	MRows, MCols := A.ColSize(), A.ColSize()
-	M := table.Random(MCols, MCols)
-
-	// Send M to yobikou
-	if err = M.ChugakuSend(&client); err != nil {
-		panic(err)
-	}
-
-	// Slice M
-	MLeft, MRight := M.Slice(0, MRows, 0, MCols/2), M.Slice(0, MRows, MCols/2, MCols)
-
-	// Calculate and send A'
-	APrime := A.Mul(MLeft)
-	if err = APrime.ChugakuSend(&client); err != nil {
-		panic(err)
-	}
-
-	// Wait and receive B'
-	BPrime, err := table.ChugakuReceive(&client)
-	if err != nil {
-		panic(err)
-	}
-
-	// Calculate and send A''
-	APrimePrime := A.Mul(MRight).Mul(BPrime)
-	if err = APrimePrime.ChugakuSend(&client); err != nil {
-		panic(err)
-	}
-
-	// Wait and receive result
-	result, err := table.ChugakuReceive(&client)
-	if err != nil {
-		panic(err)
-	}
-
-	// Export result
-	result.ExportResult("data/out/kekka.txt", "合否")
-}
-
-func yobikouSide() {
 	server, err := conn.NewYobikouServer()
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require github.com/uecconsecexp/secexp2022/se_go v0.0.0-20221202094625-3c5e9c62632a
 
-require gonum.org/v1/gonum v0.12.0 // indirect
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	gonum.org/v1/gonum v0.12.0 // indirect
+)
